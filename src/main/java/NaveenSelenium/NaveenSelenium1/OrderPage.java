aad80dda3b8822c327acaf07fde5e0c0441b29ab
leenarani <samalrani92@gmail.com>
2022-02-26 02:35:47 +0530

    orderpage addded

---
+++ src/main/java/NaveenSelenium/NaveenSelenium1/OrderPage.java
@@ -0,0 +1,11 @@
+package NaveenSelenium.NaveenSelenium1;
+
+public class OrderPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Order Page");
+
+	}
+
+}
